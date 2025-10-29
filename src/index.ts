82d0bb376b74141edef6c651f4ed445090afa377
Jonathan Adams <jonathan@connockadams.uk>
2025-10-29 20:30:08 +0000

    Remove login and logout as not required

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 
-import { loginCommand } from "./commands/login.js";
 import { scrapeCommand } from "./commands/scrape.js";
-import { logoutCommand } from "./commands/logout.js";
 
 const program = new Command();
 
 program
   .name("tcgscrape")
   .description("CLI for scraping and managing TCGCollector")
-  .addCommand(loginCommand)
-  .addCommand(logoutCommand)
   .addCommand(scrapeCommand)
   .parseAsync(process.argv);
